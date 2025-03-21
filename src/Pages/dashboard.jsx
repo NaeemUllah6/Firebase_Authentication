26209599d2336aac05ce56e9075da73b6c8805cf
naeemullah6 <naeemullahkhokhar6@gmail.com>
2025-03-21 12:29:49 +0500

    kjuhygtrf

--- src/Pages/dashboard.jsx
+++ src/Pages/dashboard.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Dashboard from '../Components/Dashboard/index'
-import DashboardLayout from '../../../userauthentication/src/Layout/DashboardLayout'
+import DashboardLayout from '../../../userauthentication/src/Layout/dashboardLayout'
 const index = () => {
   return (
       <Dashboard/>
